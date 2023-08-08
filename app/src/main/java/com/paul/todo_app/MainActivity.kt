d27a1d90367584f25b9ca32aa032749317aa0d03
PoolLuque <pluquec@unsa.edu.pe>
2023-08-07 20:11:32 -0500

    Commit task recyclerview

---
+++ app/src/main/java/com/paul/todo_app/MainActivity.kt
@@ -0,0 +1,77 @@
+package com.paul.todo_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.paul.todo_app.Categories.CategoriesAdapter
+import com.paul.todo_app.model.Task
+import com.paul.todo_app.model.TaskCategory
+import com.paul.todo_app.model.TaskCategory.*
+import com.paul.todo_app.tasks.TasksAdapter
+
+class MainActivity : AppCompatActivity() {
+    private val categories = listOf(
+        Business,
+        Personal,
+        Other
+    )
+
+    private val tasks = mutableListOf(
+        Task("Pueba tarea Bussines", Business),
+        Task("Prueba tarea personal", Personal),
+        Task("Prueba tarea otra categoria", Other)
+    )
+
+    private lateinit var rvCategories: RecyclerView
+    private lateinit var categoriesAdapter: CategoriesAdapter
+
+    private lateinit var rvTasks: RecyclerView
+    private lateinit var tasksAdapter: TasksAdapter
+    private lateinit var fabAddTask: FloatingActionButton
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initComponent()
+        initUI()
+
+    }
+
+    private fun initComponent() {
+        rvCategories = findViewById(R.id.rvCategories)
+        rvTasks = findViewById(R.id.rvTasks)
+        fabAddTask = findViewById(R.id.fabAddTask)
+    }
+
+
+    private fun initUI() {
+
+        categoriesAdapter = CategoriesAdapter(categories) { position -> updateCategories(position) }
+        rvCategories.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        rvCategories.adapter = categoriesAdapter
+
+        tasksAdapter = TasksAdapter(tasks) { position -> onItemSelected(position) }
+        rvTasks.layoutManager = LinearLayoutManager(this)
+        rvTasks.adapter = tasksAdapter
+    }
+
+    private fun updateCategories(position: Int) {
+
+    }
+
+    private fun onItemSelected(position: Int) {
+        tasks[position].isSelected = !tasks[position].isSelected
+        updateTasks()
+    }
+
+    private fun updateTasks() {
+        val selectedCategories: List<TaskCategory> = categories.filter { it.isSelected }
+        val newTasks = tasks.filter { selectedCategories.contains(it.category) }
+        tasksAdapter.tasks = newTasks
+        tasksAdapter.notifyDataSetChanged()
+    }
+
+}
